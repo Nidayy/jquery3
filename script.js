552e4c8b6575050e5135b0b54934d9303ccb1100
Merve nida yıldırım <niday642@gmail.com>
2025-07-18 18:51:22 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+  $("#veriYukleDugmesi").click(function () {
+    $.ajax({
+      url: "https://randomuser.me/api/?results=6",
+      dataType: "json",
+      success: function (apiVerisi) {
+        $("#kartListeAlani").empty();
+
+        apiVerisi.results.forEach(function (profil) {
+          const kartIcerik = `
+            <div class="kart" data-isim="${profil.name.first} ${profil.name.last}"
+                 data-eposta="${profil.email}" data-resim="${profil.picture.large}">
+              <img src="${profil.picture.medium}" alt="Profil fotoğrafı">
+              <h4>${profil.name.first} ${profil.name.last}</h4>
+              <p>${profil.email}</p>
+              <p>${profil.location.country}</p>
+            </div>
+          `;
+
+          const $profilKart = $(kartIcerik);
+          $profilKart.appendTo("#kartListeAlani").slideDown("slow");
+        });
+
+        $(".kart").hover(
+          function () {
+            $(this).fadeTo("fast", 0.85);
+          },
+          function () {
+            $(this).fadeTo("fast", 1);
+          }
+        );
+
+        $(".kart").click(function () {
+          $("#modalAd").text($(this).data("isim"));
+          $("#modalEposta").text($(this).data("eposta"));
+          $("#modalProfilFoto").attr("src", $(this).data("resim"));
+
+          $.fancybox.open({
+            src: "#kullaniciDetayModal",
+            type: "inline"
+          });
+        });
+      }
+    });
+  });
+});
